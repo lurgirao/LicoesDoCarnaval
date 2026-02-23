9318cede1e288bd79c4053b4f8db2ee5021c2e57
lurgirao <lu.rgirao@gmail.com>
2026-02-23 20:29:17 -0300

    Licoes finalizadas

--- LicoesDoCarnaval/Form1.Designer.cs
+++ LicoesDoCarnaval/Form1.Designer.cs
@@ -44,6 +44,32 @@
             label5 = new Label();
             txt_idade = new TextBox();
             button3 = new Button();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            login = new TextBox();
+            senha = new TextBox();
+            button4 = new Button();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            label14 = new Label();
+            label15 = new Label();
+            estado_civil = new ComboBox();
+            cad_nome = new TextBox();
+            cad_email = new TextBox();
+            cad_idade = new TextBox();
+            cad_end = new TextBox();
+            cad_tel = new TextBox();
+            button5 = new Button();
+            label16 = new Label();
+            label17 = new Label();
+            lanche = new TextBox();
+            label18 = new Label();
+            checkedListBox1 = new CheckedListBox();
+            confirmar_ped = new Button();
             SuspendLayout();
             // 
             // label1
@@ -199,11 +225,288 @@
             button3.UseVisualStyleBackColor = true;
             button3.Click += button3_Click;
             // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.Location = new Point(1, 274);
+            label6.Name = "label6";
+            label6.Size = new Size(136, 19);
+            label6.TabIndex = 16;
+            label6.Text = "5. Login e Senha";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Microsoft Sans Serif", 8.25F);
+            label7.Location = new Point(10, 300);
+            label7.Name = "label7";
+            label7.Size = new Size(36, 13);
+            label7.TabIndex = 17;
+            label7.Text = "Login:";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(7, 333);
+            label8.Name = "label8";
+            label8.Size = new Size(42, 15);
+            label8.TabIndex = 18;
+            label8.Text = "Senha:";
+            // 
+            // login
+            // 
+            login.Location = new Point(55, 296);
+            login.Name = "login";
+            login.Size = new Size(100, 23);
+            login.TabIndex = 19;
+            login.TextChanged += login_TextChanged;
+            // 
+            // senha
+            // 
+            senha.Location = new Point(55, 325);
+            senha.Name = "senha";
+            senha.Size = new Size(100, 23);
+            senha.TabIndex = 20;
+            senha.UseSystemPasswordChar = true;
+            senha.TextChanged += senha_TextChanged;
+            // 
+            // button4
+            // 
+            button4.BackColor = SystemColors.ScrollBar;
+            button4.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button4.Location = new Point(172, 313);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 21;
+            button4.Text = "LOGAR";
+            button4.TextImageRelation = TextImageRelation.TextBeforeImage;
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label9.Location = new Point(3, 362);
+            label9.Name = "label9";
+            label9.Size = new Size(96, 19);
+            label9.TabIndex = 22;
+            label9.Text = "6. Cadastro";
+            label9.Click += label9_Click;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Microsoft Sans Serif", 8.25F);
+            label10.Location = new Point(7, 400);
+            label10.Name = "label10";
+            label10.Size = new Size(38, 13);
+            label10.TabIndex = 23;
+            label10.Text = "Nome:";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Microsoft Sans Serif", 8.25F);
+            label11.Location = new Point(7, 426);
+            label11.Name = "label11";
+            label11.Size = new Size(38, 13);
+            label11.TabIndex = 24;
+            label11.Text = "E-mail:";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Microsoft Sans Serif", 8.25F);
+            label12.Location = new Point(8, 452);
+            label12.Name = "label12";
+            label12.Size = new Size(37, 13);
+            label12.TabIndex = 25;
+            label12.Text = "Idade:";
+            label12.Click += label12_Click;
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Font = new Font("Microsoft Sans Serif", 8.25F);
+            label13.Location = new Point(8, 479);
+            label13.Name = "label13";
+            label13.Size = new Size(64, 13);
+            label13.TabIndex = 26;
+            label13.Text = "Estado civil:";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Font = new Font("Microsoft Sans Serif", 8.25F);
+            label14.Location = new Point(7, 508);
+            label14.Name = "label14";
+            label14.Size = new Size(56, 13);
+            label14.TabIndex = 27;
+            label14.Text = "Endereço:";
+            label14.Click += label14_Click;
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Font = new Font("Microsoft Sans Serif", 8.25F);
+            label15.Location = new Point(8, 536);
+            label15.Name = "label15";
+            label15.Size = new Size(52, 13);
+            label15.TabIndex = 28;
+            label15.Text = "Telefone:";
+            // 
+            // estado_civil
+            // 
+            estado_civil.FormattingEnabled = true;
+            estado_civil.Location = new Point(78, 475);
+            estado_civil.Name = "estado_civil";
+            estado_civil.Size = new Size(102, 23);
+            estado_civil.TabIndex = 29;
+            estado_civil.SelectedIndexChanged += comboBox2_SelectedIndexChanged;
+            // 
+            // cad_nome
+            // 
+            cad_nome.Location = new Point(49, 394);
+            cad_nome.Name = "cad_nome";
+            cad_nome.Size = new Size(190, 23);
+            cad_nome.TabIndex = 30;
+            cad_nome.TextChanged += cad_nome_TextChanged;
+            // 
+            // cad_email
+            // 
+            cad_email.Location = new Point(49, 423);
+            cad_email.Name = "cad_email";
+            cad_email.Size = new Size(190, 23);
+            cad_email.TabIndex = 31;
+            cad_email.TextChanged += cad_email_TextChanged;
+            // 
+            // cad_idade
+            // 
+            cad_idade.Location = new Point(49, 448);
+            cad_idade.Name = "cad_idade";
+            cad_idade.Size = new Size(42, 23);
+            cad_idade.TabIndex = 32;
+            cad_idade.TextChanged += cad_idade_TextChanged;
+            // 
+            // cad_end
+            // 
+            cad_end.Location = new Point(69, 504);
+            cad_end.Name = "cad_end";
+            cad_end.Size = new Size(190, 23);
+            cad_end.TabIndex = 33;
+            cad_end.TextChanged += cad_end_TextChanged;
+            // 
+            // cad_tel
+            // 
+            cad_tel.Location = new Point(69, 532);
+            cad_tel.Name = "cad_tel";
+            cad_tel.Size = new Size(190, 23);
+            cad_tel.TabIndex = 34;
+            cad_tel.TextChanged += cad_tel_TextChanged;
+            // 
+            // button5
+            // 
+            button5.BackColor = SystemColors.ActiveBorder;
+            button5.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button5.Location = new Point(12, 566);
+            button5.Name = "button5";
+            button5.Size = new Size(186, 23);
+            button5.TabIndex = 35;
+            button5.Text = "VALIDAR DADOS";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button5_Click;
+            // 
+            // label16
+            // 
+            label16.AutoSize = true;
+            label16.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label16.Location = new Point(3, 611);
+            label16.Name = "label16";
+            label16.Size = new Size(83, 19);
+            label16.TabIndex = 36;
+            label16.Text = "7. Lanche";
+            label16.Click += label16_Click;
+            // 
+            // label17
+            // 
+            label17.AutoSize = true;
+            label17.Font = new Font("Microsoft Sans Serif", 8.25F);
+            label17.Location = new Point(3, 647);
+            label17.Name = "label17";
+            label17.Size = new Size(182, 13);
+            label17.TabIndex = 37;
+            label17.Text = "DIGITE O NOME DO SEU LANCHE:";
+            // 
+            // lanche
+            // 
+            lanche.Location = new Point(3, 672);
+            lanche.Name = "lanche";
+            lanche.Size = new Size(190, 23);
+            lanche.TabIndex = 38;
+            lanche.TextChanged += lanche_TextChanged;
+            // 
+            // label18
+            // 
+            label18.AutoSize = true;
+            label18.Font = new Font("Microsoft Sans Serif", 8.25F);
+            label18.Location = new Point(3, 711);
+            label18.Name = "label18";
+            label18.Size = new Size(71, 13);
+            label18.TabIndex = 39;
+            label18.Text = "ADICIONAIS:";
+            // 
+            // checkedListBox1
+            // 
+            checkedListBox1.FormattingEnabled = true;
+            checkedListBox1.Location = new Point(80, 710);
+            checkedListBox1.Name = "checkedListBox1";
+            checkedListBox1.Size = new Size(120, 76);
+            checkedListBox1.TabIndex = 40;
+            checkedListBox1.SelectedIndexChanged += checkedListBox1_SelectedIndexChanged;
+            // 
+            // confirmar_ped
+            // 
+            confirmar_ped.Location = new Point(232, 705);
+            confirmar_ped.Name = "confirmar_ped";
+            confirmar_ped.Size = new Size(75, 23);
+            confirmar_ped.TabIndex = 41;
+            confirmar_ped.Text = "CONFIRMAR PEDIDO";
+            confirmar_ped.UseVisualStyleBackColor = true;
+            confirmar_ped.Click += confirmar_ped_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 779);
+            ClientSize = new Size(800, 816);
+            Controls.Add(confirmar_ped);
+            Controls.Add(checkedListBox1);
+            Controls.Add(label18);
+            Controls.Add(lanche);
+            Controls.Add(label17);
+            Controls.Add(label16);
+            Controls.Add(button5);
+            Controls.Add(cad_tel);
+            Controls.Add(cad_end);
+            Controls.Add(cad_idade);
+            Controls.Add(cad_email);
+            Controls.Add(cad_nome);
+            Controls.Add(estado_civil);
+            Controls.Add(label15);
+            Controls.Add(label14);
+            Controls.Add(label13);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(button4);
+            Controls.Add(senha);
+            Controls.Add(login);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
             Controls.Add(button3);
             Controls.Add(txt_idade);
             Controls.Add(label5);
@@ -245,5 +548,31 @@
         private Label label5;
         private TextBox txt_idade;
         private Button button3;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private TextBox login;
+        private TextBox senha;
+        private Button button4;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private Label label13;
+        private Label label14;
+        private Label label15;
+        private ComboBox estado_civil;
+        private TextBox cad_nome;
+        private TextBox cad_email;
+        private TextBox cad_idade;
+        private TextBox cad_end;
+        private TextBox cad_tel;
+        private Button button5;
+        private Label label16;
+        private Label label17;
+        private TextBox lanche;
+        private Label label18;
+        private CheckedListBox checkedListBox1;
+        private Button confirmar_ped;
     }
 }
--- LicoesDoCarnaval/Form1.cs
+++ LicoesDoCarnaval/Form1.cs
@@ -74,7 +74,19 @@ namespace LicoesDoCarnaval
             comboBox1.Items.Add('/');
 
             comboBox1.SelectedIndex = 0;
+
+            estado_civil.Items.Add("Solteiro(a)");
+            estado_civil.Items.Add("Casado(a)");
+            estado_civil.Items.Add("Divorciado(a)");
+            estado_civil.Items.Add("Viúvo(a)");
+
+            estado_civil.SelectedIndex = 0;
+
+            string[] adicionais = { "Cebola", "Bacon", "Cheddar", "Picles" };
+            checkedListBox1.Items.AddRange(adicionais);
+
         }
+
 
         private void txt_idade_TextChanged(object sender, EventArgs e)
         {
@@ -93,9 +105,125 @@ namespace LicoesDoCarnaval
                 MessageBox.Show("Você é menor de idade");
             }
             else
-            { 
+            {
                 MessageBox.Show("Você é maior de idade");
             }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(login.Text) || string.IsNullOrEmpty(senha.Text))
+            {
+                MessageBox.Show("Preencha todos os campos");
+            }
+            else if (login.Text == "admin" && senha.Text == "1234")
+            {
+                MessageBox.Show("Login bem sucedido");
+            }
+            else
+            {
+                MessageBox.Show("Login ou senha incorretos");
+            }
+        }
+
+        private void login_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void senha_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label14_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(cad_email.Text) && !string.IsNullOrEmpty(cad_nome.Text) && !string.IsNullOrEmpty(cad_idade.Text) && !string.IsNullOrEmpty(cad_end.Text) && !string.IsNullOrEmpty(cad_tel.Text))
+            {
+                MessageBox.Show($"Cadastro realizado com sucesso!\nNome: {cad_nome.Text}\nEmail: {cad_email.Text}\nIdade: {cad_idade.Text}\nEstado Civil: {estado_civil.SelectedItem}\nEndereço: {cad_end.Text}\nTelefone: {cad_tel.Text}");
+            }
+            else
+            {
+                MessageBox.Show("Preencha todos os campos para realizar o cadastro.");
+            }
+        }
+
+        private void cad_nome_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cad_email_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cad_idade_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cad_end_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cad_tel_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label16_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lanche_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void confirmar_ped_Click(object sender, EventArgs e)
+        {         
+            string pedido = "Lanche escolhido: " + lanche.Text + "\n\n";
+                       pedido += "Adicionais:\n";
+
+            foreach (var item in checkedListBox1.CheckedItems)
+            {
+                pedido += "- " + item.ToString() + "\n";
+            }
+
+            MessageBox.Show(pedido);
         }
     }
 }
